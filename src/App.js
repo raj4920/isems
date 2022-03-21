c8a4afbf58f03554453452b208df9b613d955cb0
Raj <rajzalavadiya2000@gmail.com>
2022-03-21 18:31:19 +0530

    props type

--- src/App.js
+++ src/App.js
@@ -1,59 +1,18 @@
-import './App.css';
+import "./App.css";
+import Navbar from "./Components/Navbar";
 
 function App() {
   return (
     <>
-      <nav className="navbar navbar-expand-lg navbar-light bg-dark">
-        <div className="container-fluid">
-          <h3 className="text-light">Institute Sport Event Management</h3>
-          {/* <a className="navbar-brand " href="/">Institute Sport Event Management</a> */}
-          <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-            <span className="navbar-toggler-icon"></span>
-          </button>
-          <div className="collapse navbar-collapse" id="navbarSupportedContent">
-            <ul className="navbar-nav me-auto mb-2 mb-lg-0"></ul>
-            <ul className="nav justify-content-end">
-            <li className="nav-item">
-                <a className="nav-link active" aria-current="page" href="/">Home</a>
-              </li>
-              <li className="nav-item">
-                <a className="nav-link" href="/">Sign In</a>
-              </li>
-              <li className="nav-item">
-                <a className="nav-link" href="/" tabindex="-1" aria-disabled="true">Sign Up</a>
-              </li>
-              <li className="nav-item">
-                <a className="nav-link" href="/" tabindex="-1" aria-disabled="true">Contact Us</a>
-              </li>
-              <li className="nav-item">
-                <a className="nav-link" href="/" tabindex="-1" aria-disabled="true">About Us</a>
-              </li>
-            </ul>
-          </div>
-        </div>
-      </nav>
-      <nav className="navbar fixed-bottom navbar-expand-lg navbar-light bg-dark">
-        <div className="navbar-brand mx-auto">
-          <h6 className="text-light">@2021-22 | Bhumik Patel & Raj Zalavadiya</h6>
-        </div>
-      </nav>
+      <Navbar
+        title="Institute Sport Event Management"
+        home="Home"
+        signin="Sign In"
+        signup="Sign Up"
+        contactus="Contact Us"
+        aboutus="About Us"
+      />
     </>
-    // <div className=="App">
-    //   <header className=="App-header">
-    //     <img src={logo} className=="App-logo" alt="logo" />
-    //     <p>
-    //       Edit <code>src/App.js</code> and save to reload.
-    //     </p>
-    //     <a
-    //       className=="App-link"
-    //       href="https://reactjs.org"
-    //       target="_blank"
-    //       rel="noopener noreferrer"
-    //     >    
-    //       Hello Raj
-    //     </a>
-    //   </header>
-    // </div>
   );
 }
 
---
+++ src/Components/Navbar.js
@@ -0,0 +1,92 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+export default function Navbar(props) {
+  return (
+    <>
+      <nav className="navbar navbar-expand-lg navbar-light bg-dark">
+        <div className="container-fluid">
+          <h3 className="text-light">{props.title}</h3>
+          {/* <a className="navbar-brand " href="/">Institute Sport Event Management</a> */}
+          <button
+            className="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarSupportedContent"
+            aria-controls="navbarSupportedContent"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span className="navbar-toggler-icon"></span>
+          </button>
+          <div className="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul className="navbar-nav me-auto mb-2 mb-lg-0"></ul>
+            <ul className="nav justify-content-end">
+              <li className="nav-item">
+                <a className="nav-link active" aria-current="page" href="/">
+                  {props.home}
+                </a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" href="/">
+                  {props.signin}
+                </a>
+              </li>
+              <li className="nav-item">
+                <a
+                  className="nav-link"
+                  href="/"
+                  tabindex="-1"
+                  aria-disabled="true"
+                >
+                  {props.signup}
+                </a>
+              </li>
+              <li className="nav-item">
+                <a
+                  className="nav-link"
+                  href="/"
+                  tabindex="-1"
+                  aria-disabled="true"
+                >
+                  {props.contactus}
+                </a>
+              </li>
+              <li className="nav-item">
+                <a
+                  className="nav-link"
+                  href="/"
+                  tabindex="-1"
+                  aria-disabled="true"
+                >
+                  {props.aboutus}
+                </a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
+
+      <nav className="navbar fixed-bottom navbar-expand-lg navbar-light bg-dark">
+        <div className="navbar-brand mx-auto">
+          <h6 className="text-light">
+            @2021-22 | Bhumik Patel & Raj Zalavadiya
+          </h6>
+        </div>
+      </nav>
+    </>
+  );
+}
+
+Navbar.propTypes = {
+  title: PropTypes.string.isRequired,
+  home: PropTypes.string.isRequired,
+  signin: PropTypes.string.isRequired,
+  signup: PropTypes.string.isRequired,
+  contactus: PropTypes.string.isRequired,
+  aboutus: PropTypes.string.isRequired,
+};
+
+Navbar.defaultProps = {
+  title: "Set Title Here",
+};
