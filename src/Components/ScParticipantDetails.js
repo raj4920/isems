afb3388fa084e1d7283f35e47538b64316d82ee4
Raj <rajzalavadiya2000@gmail.com>
2022-04-07 13:49:52 +0530

    datatable

--- src/Components/ScParticipantDetails.js
+++ src/Components/ScParticipantDetails.js
@@ -1,11 +1,34 @@
 import React from "react";
 import SportCoordinatorDashboard from "./SportCoordinatorDashboard";
+import "antd/dist/antd.css";
+import ReactDOM from "react-dom";
+import DataTable from "react-data-table-component";
+import DataTableExtensions from "react-data-table-component-extensions";
+import "react-data-table-component-extensions/dist/index.css";
+import { columns, data } from "./ParticipantData";
+import "./Style.css";
 
 function ScParticipantDetails() {
+  const tableData = {
+    columns,
+    data,
+  };
   return (
     <>
       <SportCoordinatorDashboard />
-      <h1>Participant Details</h1>
+      <div className="main">
+        <DataTableExtensions {...tableData}>
+          <DataTable
+            columns={columns}
+            data={data}
+            noHeader
+            defaultSortField="id"
+            defaultSortAsc={false}
+            pagination
+            highlightOnHover
+          ></DataTable>
+        </DataTableExtensions>
+      </div>
     </>
   );
 }
